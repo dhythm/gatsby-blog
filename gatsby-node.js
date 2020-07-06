ab2d5bfcf6c7ef9b82bfed1a5b9bd76b0208c8e2
dhythm <Yuta.Okada.20@gmail.com>
2020-07-06 23:48:29 +0900

    add templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,34 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  const postTemplate = path.resolve("src/templates/blogTemplate.js")
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) {
+      return Promise.reject(res.errors)
+    }
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+      })
+    })
+  })
+}
